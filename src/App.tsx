b136d9ea1f4fd6806fbb6a8e4bef7a4c84c93d65
Fraser Benjamin <fraser.benjamin@outlook.com>
2021-08-07 16:16:22 +0100

    Added code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
-import SelectAttacks from './components/randomAttacks/SelectAttacks';
-import Play from './components/randomAttacks/Play';
 import AppContext from './context/appContext';
+
+const SelectAttacks = lazy(() => import('./components/randomAttacks/SelectAttacks'));
+const Play = lazy(() => import('./components/randomAttacks/Play'));
+
+const renderLoader = () => null;
 
 const App = () => {
   const [timeline, setTimeline] = useState<number[]>([]);
@@ -21,14 +24,16 @@ const App = () => {
         }}
       >
         <div className="flex flex-col bg-gray-100 items-center w-full h-full fixed overflow-y-auto">
-          <Switch>
-            <Route exact path="/">
-              <SelectAttacks />
-            </Route>
-            <Route path="/play">
-              <Play />
-            </Route>
-          </Switch>
+          <Suspense fallback={renderLoader()}>
+            <Switch>
+              <Route exact path="/">
+                <SelectAttacks />
+              </Route>
+              <Route path="/play">
+                <Play />
+              </Route>
+            </Switch>
+          </Suspense>
         </div>
       </AppContext.Provider>
     </Router>
