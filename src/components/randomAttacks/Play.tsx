ddf3c2fe8d03535071d0e1363c9911be9ef1b2a5
Fraser Benjamin <fraser.benjamin@outlook.com>
2021-08-11 19:54:48 +0100

    Added exit confirmation

--- src/components/randomAttacks/Play.tsx
+++ src/components/randomAttacks/Play.tsx
@@ -6,15 +6,16 @@ import ViewAttack from './ViewAttack';
 import { groups } from "../../assets/randomAttacks/groups";
 import { numericArraysEqual } from '../../common/util';
 import { IGroup } from '../../types';
-import { FaHome } from "react-icons/fa";
+import { RiArrowGoBackLine } from "react-icons/ri";
 
 const Play = () => {
   const history = useHistory();
   const { timeline } = useContext(AppContext);
   const [pointer, setPointer] = useState<number>(0);
+  const [modal, setModal] = useState<boolean>(false);
   const scrollRef = useRef<HTMLDivElement>(null);
 
-  if(timeline.length === 0) history.push("/");
+  if (timeline.length === 0) history.push("/");
 
   const filteredTimeline = timeline.slice(0, pointer + 1);
 
@@ -49,29 +50,49 @@ const Play = () => {
   }
 
   return (
-    <div className="flex flex-col w-full h-full overflow-visible">
-      <div ref={scrollRef} className="scroll-snap-y flex flex-col overflow-y-auto flex-grow hide-scrollbar">
-        {filteredTimeline.map((item: number, i: number) => (
-          <ViewAttack key={i} attack={item} index={i + 1} total={timeline.length} />
-        ))}
-      </div>
+    <>
+      {modal ? (
+        <div className="bg-gray-900 bg-opacity-80 w-full h-full absolute top-0 left-0 z-20 flex justify-center">
+          <div className="bg-white rounded self-center py-3 px-6">
+            <div>Are you sure you want to exit?</div>
+            <div className="mt-3 flex justify-end space-x-2">
+              <button
+                className="py-2 px-4 rounded-lg transition-colors duration-200 font-medium text-gray-700 hover:bg-gray-100"
+                onClick={() => setModal(false)}
+              >Cancel</button>
+              <button
+                className="py-2 px-4 rounded-lg transition-colors duration-200 font-medium text-white bg-red-500 hover:bg-red-600"
+                onClick={() => history.push("/")}
+              >Exit</button>
+            </div>
+          </div>
+        </div>
+      ) : null}
 
-      <div className="flex w-full space-x-3 p-3">
-        <button
-          className="py-3 px-4 rounded-lg transition-colors duration-200 font-medium bg-gray-500 hover:bg-gray-600"
-          onClick={() => history.push("/")}
-        >
-          <FaHome color="white" size={24}/>
-        </button>
-        <button
-          disabled={pointer + 1 >= timeline.length}
-          className={`py-3 px-6 text-white rounded-lg transition-colors duration-200 font-medium ${activeGroup().baseColour} hover:${activeGroup().hoverColour} flex-grow disabled:bg-gray-200 disabled:text-gray-700 disabled:cursor-default`}
-          onClick={nextItem}
-        >
-          New Attack
-        </button>
+      <div className="flex flex-col w-full h-full overflow-visible">
+        <div ref={scrollRef} className="scroll-snap-y flex flex-col overflow-y-auto flex-grow hide-scrollbar">
+          {filteredTimeline.map((item: number, i: number) => (
+            <ViewAttack key={i} attack={item} index={i + 1} total={timeline.length} />
+          ))}
+        </div>
+
+        <div className="flex w-full space-x-3 p-3">
+          <button
+            className="py-3 px-4 rounded-lg transition-colors duration-200 font-medium bg-gray-500 hover:bg-gray-600"
+            onClick={() => setModal(true)}
+          >
+            <RiArrowGoBackLine color="white" size={24} />
+          </button>
+          <button
+            disabled={pointer + 1 >= timeline.length}
+            className={`py-3 px-6 text-white rounded-lg transition-colors duration-200 font-medium ${activeGroup().baseColour} hover:${activeGroup().hoverColour} flex-grow disabled:bg-gray-200 disabled:text-gray-700 disabled:cursor-default`}
+            onClick={nextItem}
+          >
+            New Attack
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
