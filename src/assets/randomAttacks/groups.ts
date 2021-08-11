ddca27854c0aeda316a14b941b56e2e2d282e42d
Fraser Benjamin <fraser.benjamin@outlook.com>
2021-08-11 20:44:47 +0100

    Updated order

--- src/assets/randomAttacks/groups.ts
+++ src/assets/randomAttacks/groups.ts
@@ -1,13 +1,5 @@
 export const groups = [
   {
-    "title": "Clear",
-    "baseColour": "bg-red-500",
-    "hoverColour": "hover:bg-red-600",
-    "textColour": "text-white",
-    "borderColour": "border-white",
-    "attacks": []
-  },
-  {
     "title": "Green Belt",
     "baseColour": "bg-green-600",
     "hoverColour": "hover:bg-green-700",
@@ -30,5 +22,13 @@ export const groups = [
     "borderColour": "border-gray-800",
     "textColour": "text-white",
     "attacks" :[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+  },
+  {
+    "title": "Clear",
+    "baseColour": "bg-red-500",
+    "hoverColour": "hover:bg-red-600",
+    "textColour": "text-white",
+    "borderColour": "border-white",
+    "attacks": []
   }
 ]
